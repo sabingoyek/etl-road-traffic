174d04b5f051864efae9a175b7570678d9b2e5f4
Kimba SABI N'GOYE <kimbasabingoye@gmail.com>
2023-05-09 20:57:09 +0100

    email address removed

--- staging/ETL_toll_data.py
+++ staging/ETL_toll_data.py
@@ -10,7 +10,7 @@ from airflow.utils.dates import days_ago
 default_args = {
     'owner': "Kimba SABI N'GOYE",
     'start_date': days_ago(0),
-    'email': ['kimbasabingoye@gmail.com'],
+    'email': ['******'],
     'email_on_failure': True,
     'email_on_retry': True,
     'retries': 1,
